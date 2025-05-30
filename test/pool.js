5408b412587ad959d7868dd0a5f0c04e153b1862
Patrick Thomson <patrick.thomson@bubble.io>
2025-05-30 10:27:14 -0400

    Restore async resources so that `AsyncLocalStorage` can work.
    As currently implemented, this package doesn't restore the async resources of an execution context across
    calls to `Fiber.yield`. This leads to mysterious crashes when trying to use `AsyncLocalStorage` in apps that
    use fibers. I got this patch from https://github.com/laverdet/node-fibers/pull/466. The added tests appear to
    work; I'm going to try this with my local copy and see if the crashes are ameliorated.

--- test/pool.js
+++ test/pool.js
@@ -1,4 +1,5 @@
 var Fiber = require('fibers');
+const { AsyncLocalStorage } = require('async_hooks');
 
 for (var jj = 0; jj < 10; ++jj) {
 	var fibers = [];
@@ -13,4 +14,41 @@ for (var jj = 0; jj < 10; ++jj) {
 		fibers[ii].run();
 	}
 }
-console.log('pass');
+
+var result = 'pass';
+var _storage = new AsyncLocalStorage();
+if (typeof _storage.getStore() !== 'undefined') {
+	result = 'fail';
+	throw new Error('storage should be undefined outside of .run call');
+}
+_storage.run(123, function() {
+	if (_storage.getStore() !== 123) {
+		result = 'fail';
+		throw new Error('store should persist values');
+	}
+});
+
+_storage.run(456, function() {
+	if (_storage.getStore() !== 456) {
+		result = 'fail';
+		throw new Error('store should persist new values');
+	}
+})
+
+_storage.run({}, function() {
+	_storage.getStore()['doodad'] = 456;
+	if (_storage.getStore()['doodad'] !== 456) {
+		result = 'fail';
+		throw new Error('mutable stores should reflect changes');
+	}
+})
+
+_storage.run(789, function() {
+	_storage.disable();
+	if (typeof _storage.getStore() !== 'undefined') {
+		result = 'fail';
+		throw new Error('.disable should make .getStore calls in .run return undefined');
+	}
+})
+
+console.log(result);
